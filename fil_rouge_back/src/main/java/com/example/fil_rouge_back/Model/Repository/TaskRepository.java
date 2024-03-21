eef80cd0ffc98c7326f7cfcd516f4d6997c2eb4f
CyrilFreeman <117688908+CyrilFreeman@users.noreply.github.com>
2024-03-21 11:52:08 +0100

    resolve merging conflict in repo

--- fil_rouge_back/src/main/java/com/example/fil_rouge_back/Model/Repository/TaskRepository.java
+++ fil_rouge_back/src/main/java/com/example/fil_rouge_back/Model/Repository/TaskRepository.java
@@ -7,10 +7,8 @@ import org.springframework.stereotype.Repository;
 import java.util.Optional;
 
 @Repository
-<<<<<<< HEAD
-public interface TaskRepository extends JpaRepository<TaskEntity, Long> {}
-=======
-public interface TaskRepository extends JpaRepository<Task, Long> {
-    Optional<Task> findByTitle(String title);
+
+public interface TaskRepository extends JpaRepository<TaskEntity, Long> {
+    Optional<TaskEntity> findByTitle(String title);
 }
->>>>>>> 1db4a4e4505af9af16ae92449664ef2938f1a82e
+
---
+++ fil_rouge_back/src/main/java/com/example/fil_rouge_back/Service/ProjectService.java
@@ -0,0 +1,2 @@
+package com.example.fil_rouge_back.Service;public class ProjectService {
+}
