2b2249dcc1f50ec763275057590d8624ba963a9e
CyrilFreeman <117688908+CyrilFreeman@users.noreply.github.com>
2024-03-21 15:09:32 +0100

    entity is done

--- fil_rouge_back/src/main/java/com/example/fil_rouge_back/Model/Entity/Project.java
+++ fil_rouge_back/src/main/java/com/example/fil_rouge_back/Model/Entity/Project.java
@@ -5,6 +5,7 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.springframework.context.annotation.Lazy;
 import org.springframework.scheduling.config.Task;
 
 import java.util.HashSet;
@@ -15,17 +16,25 @@ import java.util.Set;
 @Getter
 @Setter
 @NoArgsConstructor
-@AllArgsConstructor
 public class Project {
     @Id
     @GeneratedValue(strategy = GenerationType.TABLE)
     private Long id;
+
+
 
     private String name;
     private Long userId;
     @OneToMany(mappedBy = "project",fetch = FetchType.LAZY,cascade = CascadeType.ALL)
 
     private Set<TaskEntity> tasks = new HashSet<>();
+
+    public Project(Long id, String name, Long userId,@Lazy Set<TaskEntity> tasks) {
+        this.id = id;
+        this.name = name;
+        this.userId = userId;
+        this.tasks = tasks;
+    }
 }
 
 
