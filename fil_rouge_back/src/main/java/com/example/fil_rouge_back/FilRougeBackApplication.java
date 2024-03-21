772befc45cfce462858b026b1924363ab4a56487
Elise <elise.maaiza33@gmail.com>
2024-03-21 09:35:38 +0100

    LET'S GO git add .!

---
+++ fil_rouge_back/src/main/java/com/example/fil_rouge_back/FilRougeBackApplication.java
@@ -0,0 +1,13 @@
+package com.example.fil_rouge_back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FilRougeBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FilRougeBackApplication.class, args);
+	}
+
+}
---
+++ fil_rouge_back/src/test/java/com/example/fil_rouge_back/FilRougeBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.fil_rouge_back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FilRougeBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
