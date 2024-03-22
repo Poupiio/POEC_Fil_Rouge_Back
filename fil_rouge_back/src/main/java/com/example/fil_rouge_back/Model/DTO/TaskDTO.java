343360486e0092016f6b19e255471b8c212dbf2c
Elise <elise.maaiza33@gmail.com>
2024-03-22 10:57:54 +0100

    create DTO and fixe CRUC for task

--- fil_rouge_back/src/main/java/com/example/fil_rouge_back/Model/DTO/TaskDTO.java
+++ fil_rouge_back/src/main/java/com/example/fil_rouge_back/Model/DTO/TaskDTO.java
@@ -1,2 +1,23 @@
-package com.example.fil_rouge_back.Model.DTO;public class TaskDTO {
+package com.example.fil_rouge_back.Model.DTO;
+
+import com.example.fil_rouge_back.Model.Entity.Status;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class TaskDTO {
+    private Long id;
+    private String title;
+    private String description;
+    @Enumerated(EnumType.STRING)
+    private Status status;
+    private Float estimationHours;
 }
--- fil_rouge_back/src/main/java/com/example/fil_rouge_back/Model/Repository/TaskRepository.java
+++ fil_rouge_back/src/main/java/com/example/fil_rouge_back/Model/Repository/TaskRepository.java
@@ -4,11 +4,15 @@ import com.example.fil_rouge_back.Model.Entity.TaskEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 
 public interface TaskRepository extends JpaRepository<TaskEntity, Long> {
     Optional<TaskEntity> findByTitle(String title);
+
+    // Fonction pour récupérer les tâches associées à un projet spécifique (id)
+    List<TaskEntity> findAllTasksByProjectId(Long projectId);
 }
 
